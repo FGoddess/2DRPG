83f74aa7bd5b0b288605ca4de18f1de72c9d1fd3
Nick <kolan.eremeev1001@gmail.com>
2021-01-19 21:09:48 +0300

    First push.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y+2, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _playerRB;
+    [SerializeField] private BoxCollider2D _groundCheck;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] public bool _isOnGround;
+    
+    void Start()
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        _playerRB.velocity = new Vector2(horizontalInput * _moveSpeed, _playerRB.velocity.y);
+        Jump();
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        {
+            _playerRB.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    
+}

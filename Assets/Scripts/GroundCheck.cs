19cb1c8869993c0a5d47ae6c19b5b488fca8d2ff
Nick <kolan.eremeev1001@gmail.com>
2021-01-20 18:24:27 +0300

    AAAAA

--- Assets/Scripts/GroundCheck.cs
+++ Assets/Scripts/GroundCheck.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundCheck : MonoBehaviour
 {
+    public static Action<bool> OnGroundCheck;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.GetComponentInParent<PlayerController>()._isOnGround = true;
+        if (OnGroundCheck != null)
+            OnGroundCheck(true);
         
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        gameObject.GetComponentInParent<PlayerController>()._isOnGround = false;
+        if (OnGroundCheck != null)
+            OnGroundCheck(false);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,37 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _playerRB;
     [SerializeField] private BoxCollider2D _groundCheck;
+    [SerializeField] private Animator _playerAnim;
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _jumpForce;
     [SerializeField] public bool _isOnGround;
-    
+
     void Start()
     {
         _playerRB = GetComponent<Rigidbody2D>();
+        _playerAnim = GetComponent<Animator>();
     }
 
     void Update()
     {
         var horizontalInput = Input.GetAxis("Horizontal");
-
-        _playerRB.velocity = new Vector2(horizontalInput * _moveSpeed, _playerRB.velocity.y);
+        Movement(horizontalInput);
         Jump();
+    }
+
+    private void Movement(float horizontalInput)
+    {
+        _playerRB.velocity = new Vector2(horizontalInput * _moveSpeed, _playerRB.velocity.y);
+
+        if (horizontalInput > 0)
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        else if (horizontalInput < 0)
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+
+        if (Mathf.Abs(horizontalInput) > Mathf.Epsilon)
+            _playerAnim.SetInteger("State", 1);
+        else 
+            _playerAnim.SetInteger("State", 0);
     }
 
     private void Jump()
@@ -31,5 +47,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    
+    private void OnEnable()
+    {
+        GroundCheck.OnGroundCheck += SetGround;
+    }
+
+    private void OnDisable()
+    {
+        GroundCheck.OnGroundCheck -= SetGround;
+    }
+
+    private void SetGround(bool temp)
+    {
+        _isOnGround = temp;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Spikes"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
 }
